52f92afb95e6689e93a04f0d9744f8e7ec7aab13
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-19 15:30:34 +0800

    fin install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GREEN=10
-RED=9
-TEAL=6
 WHITE=15
-export GUM_INPUT_PROMPT_FOREGROUND="$GREEN"
+BLACK=0
+GREEN=10
+BLUE=14
+PURPLE=13
+RED=9
+
+export GUM_INPUT_PROMPT_FOREGROUND="$PURPLE"
 export GUM_INPUT_CURSOR_FOREGROUND=""
 export GUM_INPUT_HEADER_FOREGROUND=""
-export GUM_CHOOSE_HEADER_FOREGROUND="$GREEN"
+export GUM_CHOOSE_HEADER_FOREGROUND="$PURPLE"
 export GUM_CHOOSE_SELECTED_FOREGROUND="$GREEN"
 export GUM_CHOOSE_CURSOR_FOREGROUND="$GREEN"
-export GUM_CONFIRM_PROMPT_FOREGROUND="$GREEN"
+export GUM_CONFIRM_PROMPT_FOREGROUND="$PURPLE"
 export GUM_CONFIRM_SELECTED_BACKGROUND="$GREEN"
 export GUM_CONFIRM_UNSELECTED_BACKGROUND=""
 export GUM_CONFIRM_UNSELECTED_FOREGROUND=""
@@ -27,7 +30,7 @@ done
 
 # Partition and format the disk
 clear
-gum style "Using Arch again, Rajdeep? Let's set up your user account..." --foreground "$TEAL"
+gum style "Using Arch again, Rajdeep? Let's set up your user account..." --foreground "$BLUE"
 echo ""
 while true; do
     pass=$(gum input --password --placeholder "Used for user + root + encryption" --prompt "Password> ")
@@ -40,7 +43,7 @@ while true; do
 done
 
 clear
-gum style "Great! Now let's select where to install Arch..." --foreground "$TEAL"
+gum style "Great! Now let's select where to install Arch..." --foreground "$BLUE"
 echo ""
 while true; do
     disk=$(lsblk -dpno NAME,SIZE,ROTA,TYPE | awk '$3==0 && $4=="disk" {print $1, $2}' | gum choose --header "Select install disk" | awk '{print $1}')
@@ -69,5 +72,5 @@ while true; do
 done
 
 clear
-gum style "Disk overwritten! Installing Arch..." --foreground "$TEAL"
+gum style "Disk overwritten! Installing Arch..." --foreground "$BLUE"
 echo ""
