f0af18fb5a9472697212696e147110326fe3cb8e
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-24 10:52:27 +0800

    fin arch-or-install

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set fish_greeting
-fish_add_path $HOME/Repositories/arch-dotfiles/bin
 
 if status is-interactive
     if not ssh-add -l &>/dev/null
