33ab40505207dd5d105177245f223e50e43161be
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-28 04:25:31 +0800

    contd nvim

--- config/nvim/.config/nvim/lua/plugins/plugin.lua
+++ config/nvim/.config/nvim/lua/plugins/plugin.lua
@@ -8,4 +8,23 @@ return {
       vim.cmd([[colorscheme catppuccin-mocha]])
     end,
   },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+    ---@type snacks.Config
+    opts = {
+      notifier = { enabled = true },
+      explorer = { enabled = true },
+    },
+  }
 }
