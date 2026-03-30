5dd6c04c117e45ac628249efdee050ae24ac8824
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-30 14:18:56 +0800

    add tnm eza

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,6 +1,11 @@
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set fish_greeting
 
+alias ls 'eza -lh --group-directories-first --icons=auto'
+alias lsa 'ls -a'
+alias lt 'eza --tree --level=2 --long --icons --git'
+alias lta 'lt -a'
+
 if status is-interactive
     if not ssh-add -l &>/dev/null
         ssh-add $HOME/.ssh/rajdeepsh
