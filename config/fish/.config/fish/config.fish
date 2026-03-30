5c8b665358ec06d6df1479456baca6c9285e15fe
Rajdeep Singh Hundal <rajd33psh@gmail.com>
2026-03-30 18:47:50 +0800

    fix ls and lsa for eza

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set fish_greeting
 
-alias ls 'eza -lh --group-directories-first --icons=auto'
+alias ls 'eza -lh -F never --color=always --icons=always --group-directories-first --show-symlinks -s name'
 alias lsa 'ls -a'
 alias lt 'eza --tree --level=2 --long --icons --git'
 alias lta 'lt -a'
